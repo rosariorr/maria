1997257ab60abc2dcd6fba886345218bec511e05
rosario <rosariohuachua@gmail.com>
2019-10-26 11:44:53 -0500

    cuarto cambio

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,5 +1,7 @@
 -- creacion de tablas SQL
+-- Trabajando en rama alterna - desarrollo - etiqueta nro 4
 create table td_cliente (
 nombre varchar2(50)
 apellidos varchar2 (50)
 Direccion varchar2(100))
+tablespace;
